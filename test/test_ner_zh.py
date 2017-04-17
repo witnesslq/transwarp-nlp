d8b3bda9d8fb228fbe2f55416b5152f24e4b9cdd
endymecy <endymecy@sina.cn>
2017-04-17 14:43:18 +0800

    init

---
+++ test/test_ner_zh.py
@@ -0,0 +1,24 @@
+#coding:utf-8
+from __future__ import unicode_literals # compatible with python3 unicode
+
+from transwarpnlp import segmenter
+from transwarpnlp import ner_tagger
+tagger = ner_tagger.load_model(lang = 'zh')
+
+#Segmentation
+text = "我爱吃北京烤鸭"
+words = segmenter.seg(text)
+print (" ".join(words).encode('utf-8'))
+
+#NER tagging
+tagging = tagger.predict(words)
+for (w,t) in tagging:
+    str = w + "/" + t
+    print (str.encode('utf-8'))
+
+#Results
+#我/nt
+#爱/nt
+#吃/nt
+#北京/p
+#烤鸭/nt
